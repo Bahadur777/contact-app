88a0328b2f86b73e3477378540dcbda6fdaadafd
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2023-08-27 13:26:56 +0600

    Starting Navbar and setup firebase database

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css'
+import Navbar from './conponents/Navbar'
 
 function App() {
   
   return (
     <>
-     <h1 className="text-3xl font-bold underline bg-red-500 ">
-      Hello world!
-    </h1>
+     <Navbar/>
     </>
   )
 }
