23c281783a031beef78371227696e69e2c470e61
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2023-08-29 22:09:51 +0600

    working in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import Navbar from './conponents/Navbar'
-import Search from './conponents/Search'
+import Navbar from './components/Navbar'
+import Search from './components/Search'
+import{collection, getDocs} from "firebase/firestore"
+import {db} from "./confing/firebase"
 
 function App() {
    
   const [contacts, setContacts] = useState([]);
+  useEffect(()=>{
+    const getContacts = async()=>{
+      try {
+        const contactsCollection = collection(db,"contacts-app");
+        const contactsSnap = await getDocs(contactsCollection);
+        console.log(contactsSnap);
+        const contactLists = contactsSnap.docs.map((docs)=>{
+          return{
+            id: docs.id,
+            ...docs.data()
+          }
+        })
+        console.log(contactLists);
+      } catch(error){
+        console.log(error)
+      }
+    }
+  },[])
 
 
 
